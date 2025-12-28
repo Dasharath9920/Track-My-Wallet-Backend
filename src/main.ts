c3b8fd57d41bedbe2c6d4fba12293103923ce75d
Dasharath Dyavari <dyavaridasharath82@gmail.com>
2025-12-28 17:11:55 +0000

    fix(app): add frontend url

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
-  })
+    origin: ['http://localhost:5173', 'https://track-my-wallet.vercel.app'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
